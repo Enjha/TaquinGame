f1a8399d02e1a40e56ab72d6e03e6c1bc921f2f3
Enjha <proxzy-yann@hotmail.fr>
2021-02-01 14:07:47 +0100

    classe Grid

--- taquinGame/src/Main.java
+++ taquinGame/src/Main.java
@@ -1,18 +1,7 @@
-import ia.GridGame;
-
-import java.util.Scanner;
-
 public class Main {
 
     public static void main(String[] args){
-        int line, column;
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Choisissez le nombre de ligne entre 2 et 5:");
-        line = scanner.nextInt();
-        System.out.println("Choisissez le nombre de colonnes entre 2 et 5:");
-        column = scanner.nextInt();
-        GridGame grid = new GridGame(line,column);
-        grid.affiche();
+
     }
 
 }
--- taquinGame/src/ia/GridGame.java
+++ taquinGame/src/ia/GridGame.java
@@ -1,16 +1,14 @@
 package ia;
 
-import java.io.*;
-
 public class GridGame {
     private final int n;
     private final int m;
-    private char[][] values;
+    private final char[][] values;
 
     public GridGame(int n, int m) {
         this.n = n;
         this.m = m;
-        this.values = paddingGrid();
+        this.values = new char[n][m];
     }
 
     public int getN() {return n;}
@@ -21,35 +19,12 @@ public class GridGame {
         this.values[x][y] = values;
     }
 
-    public char[][] paddingGrid(){
-        this.values = new char[n][m];
-        try {
-            File f = new File("C:\\Users\\proxz\\Desktop\\TaquinGame\\taquinGame\\src\\ressources\\taquin_"+this.n+"x"+this.m+".grid");
-            BufferedReader b = new BufferedReader(new FileReader(f));
-            String readLine;
-            int ligne = 0;
-            while ((readLine = b.readLine()) != null) {
-                    for (int i = 0; i <= m - 1; i++) {
-                        if(i<=readLine.length()-1)
-                            values[ligne][i] = readLine.charAt(i);
-                        else
-                            values[ligne][i] = ' ';
-                    }
-                if(ligne == n-1) break;
-                ligne++;
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return values;
-    }
-
-    public void affiche(){
+    public void printValues(){
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
-                System.out.println(values[i][j]);
+                System.out.print(values[i][j]);
             }
-            System.out.println("\n");
+            System.out.println();
         }
     }
 
