5ac9a061b420aee4cc9ba29689a95d1d13317180
Enjha <proxzy-yann@hotmail.fr>
2021-02-26 22:04:16 +0100

    Avancement de l'algorithme en profondeur

--- taquinGame/src/algorithm/DeepRouteAlgo.java
+++ taquinGame/src/algorithm/DeepRouteAlgo.java
@@ -1,4 +1,43 @@
 package algorithm;
 
+import ia.GridState;
+import ia.TaquinGame;
+
+import java.util.ArrayList;
+
 public class DeepRouteAlgo {
+
+    private TaquinGame taquinGame;
+
+    public DeepRouteAlgo(TaquinGame taquinGame){
+        this.taquinGame = taquinGame;
+    }
+
+    public TaquinGame getTaquinGame(){
+        return taquinGame;
+    }
+
+    public boolean algorithmeDeep(){
+        GridState initialState = taquinGame.getInitialState();
+        GridState finalState = taquinGame.getFinalState();
+        GridState currentState;
+        ArrayList<GridState> closedState = new ArrayList<>();
+        ArrayList<GridState> openState = new ArrayList<>();
+        openState.add(initialState);
+
+        while(openState.size() > 0 && openState.get(openState.size()-1) != finalState){
+            currentState = openState.get(openState.size()-1);
+            openState.remove(currentState);
+            closedState.add(currentState);
+            /* - générer tous les voisins possibles de e et les insérer dans Ouvert
+                    s'ils ne sont pas déjà dans Ouvert [ni dans Ferme´] - */
+
+        }
+        if(openState.size() == 0)
+            return false;
+        else{
+            return false;
+        }
+    }
+
 }
