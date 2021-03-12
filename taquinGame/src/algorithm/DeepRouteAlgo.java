a9fd4908edb8a21b88546dff2294863ab7627393
Enjha <proxzy-yann@hotmail.fr>
2021-03-12 18:47:37 +0100

    Algorithme en profondeur fini( à vérifier )

--- taquinGame/src/algorithm/DeepRouteAlgo.java
+++ taquinGame/src/algorithm/DeepRouteAlgo.java
@@ -7,7 +7,7 @@ import java.util.ArrayList;
 
 public class DeepRouteAlgo {
 
-    private TaquinGame taquinGame;
+    private final TaquinGame taquinGame;
 
     public DeepRouteAlgo(TaquinGame taquinGame){
         this.taquinGame = taquinGame;
@@ -31,6 +31,12 @@ public class DeepRouteAlgo {
             closedState.add(currentState);
             /* - générer tous les voisins possibles de e et les insérer dans Ouvert
                     s'ils ne sont pas déjà dans Ouvert [ni dans Ferme´] - */
+            ArrayList<GridState> negihbors = currentState.generateNeighbors();
+            for(GridState grid : negihbors){
+                if(!openState.contains(grid) && !closedState.contains(grid)){
+                    openState.add(grid);
+                }
+            }
 
         }
         if(openState.size() == 0)
--- taquinGame/src/ia/GridState.java
+++ taquinGame/src/ia/GridState.java
@@ -1,5 +1,7 @@
 package ia;
 
+import java.util.ArrayList;
+
 public class GridState {
     private final int nbLine;
     private final int nbColumn;
@@ -26,4 +28,30 @@ public class GridState {
         return caseVide;
     }
 
+    public ArrayList<GridState> generateNeighbors(){
+        ArrayList<GridState> neighbors = new ArrayList<>();
+        int x = caseVide.getPosX();
+        int y = caseVide.getPosY();
+        if ((x-1) >= 0){
+            char[][] valuesTemp1 = values;
+            valuesTemp1[x][y] = values[x-1][y];
+            neighbors.add(new GridState(nbLine,nbColumn,valuesTemp1,new VoidCase(x-1,y)));
+        }
+        if((x+1) <= (nbColumn - 1)){
+            char[][] valuesTemp2 = values;
+            valuesTemp2[x][y] = values[x-1][y];
+            neighbors.add(new GridState(nbLine,nbColumn,valuesTemp2,new VoidCase(x+1,y)));
+        }
+        if((y-1) >= 0){
+            char[][] valuesTemp3 = values;
+            valuesTemp3[x][y] = values[x-1][y];
+            neighbors.add(new GridState(nbLine,nbColumn,valuesTemp3,new VoidCase(x,y-1)));
+        }
+        if((y+1) <= (nbLine - 1)){
+            char[][] valuesTemp4 = values;
+            valuesTemp4[x][y] = values[x-1][y];
+            neighbors.add(new GridState(nbLine,nbColumn,valuesTemp4,new VoidCase(x,y+1)));
+        }
+        return neighbors;
+    }
 }
