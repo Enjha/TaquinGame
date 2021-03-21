536d9417243792ddb11437188e50eb4bbaf3d5dc
Enjha <proxzy-yann@hotmail.fr>
2021-03-21 16:56:33 +0100

    3 types de recherches implémentez + 3 heuristiques + affichages des résulats

--- taquinGame/src/Main.java
+++ taquinGame/src/Main.java
@@ -1,4 +1,5 @@
 import algorithm.AlgorithmEnumeration;
+import fx.InterfaceSwing;
 import ia.Initialization;
 import ia.TaquinGame;
 
@@ -10,6 +11,7 @@ public class Main {
 
     public static void main(String[] args) throws URISyntaxException {
 
+        new InterfaceSwing().setVisible(true);
         Initialization initialization = new Initialization(AlgorithmEnumeration.BestOneSearch);
         TaquinGame taquinGame = initialization.initialize();
         taquinGame.start();
